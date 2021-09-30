d740a81d623763bba6d8dc2177dd865980e970bf
matias-selzer <matias.selzer@gmail.com>
2021-09-30 17:07:01 -0300

    MVP implementado caso agregar automotor

--- Assets/MVP/SinMVP/Scripts/Manager.cs
+++ Assets/MVP/SinMVP/Scripts/Manager.cs
@@ -3,31 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Manager : MonoBehaviour
 {
     public InputField inputAgregarAutomotor, inputConsultarDeudas, inputPagarDeudas;
     public Text mensajeAgregarAutomotor, mensajeConsultarDeudas, mensajePagarDeudas;
 
-    public void AgregarAutomotor()
-    {
-        string patente = inputAgregarAutomotor.text;
-        bool automotorYaExiste=false;
-        //guardar automotor en la base de datos, etc etc
-        MostrarMensajeAgregarAutomotor(automotorYaExiste);
-    }
-
-    public void MostrarMensajeAgregarAutomotor(bool automotorYaExiste)
-    {
-        if (automotorYaExiste)
-        {
-            mensajeAgregarAutomotor.text = "El automotor ya existe en la base de datos.";
-        }
-        else
-        {
-            mensajeAgregarAutomotor.text = "El automotor ha sido agregado satisfactoriamente.";
-        }
-    }
-
+    
     //**************************************************************************************
 
     public void ConsultarDeudas()
@@ -64,4 +46,5 @@ public class Manager : MonoBehaviour
         mensajePagarDeudas.text = "Las deudas han sido pagadas.";
     }
 
+    //**************************************************************************************
 }
